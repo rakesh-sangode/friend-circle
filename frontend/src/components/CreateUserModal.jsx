08a3a664b4e6b5b324a05deea35d0041c9cb7f17
rakesh_sangode <rakeshsangode97@gmail.com>
2025-01-07 18:44:09 +0530

    frontend setup complete with react and chakra ui library

---
+++ frontend/src/components/CreateUserModal.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const CreateUserModal = () => {
+  return (
+    <>
+      <Button>Create User</Button>
+    </>
+  )
+}
+
+export default CreateUserModal
